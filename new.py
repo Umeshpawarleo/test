5c91ad732be3cc8a1b20845d5b4ceb3d003b9d53
umeshpawarleo <umeshpawarleo8@gmail.com>
2023-10-14 14:17:15 +0530

    test

---
+++ new.py
@@ -0,0 +1,37 @@
+print ("Hello World")
+ priciple=input("Enterprinciple amount")
+ interest = input("enter rate of interest")
+ years = input("No. of years")
+
+ print ("simple Interest = "+ (princple*interest*years)/100)
+
+
+ cars=int(input("provide the number of cars"))
+ bikes=int(input("provide the number of bikes"))
+
+ totalCollection= (cars*20*6)+(bikes*10*6)
+
+if(totalCollection>10000):
+    print("its a Profitable day")
+elif(totalCollection == 10000)
+    print("fair day")
+else:
+    print("Its a bad Day")
+
+
+## for loop
+for i in range(0,5,1):
+    print(i)
+
+#printing odd numbers and calculating there sum
+sum = 0
+limit=int(input("Provide the limit value"))
+for i in range(0,limit,1):
+    if(i%2=1):
+        print(i)
+        sum=sum+i
+        
+print("sum = ",sum)
+
+
+
